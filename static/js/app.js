6fbb3400a910883c3123928f20b156b0d1fa1e41
12wollmana <awollman2@gmail.com>
2020-09-28 16:43:30 -0500

    Finished Bar Chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,17 @@
+/**
+ * Filepath to the JSON file for the data samples.
+ */
 const samplesJsonFile = "static/data/samples.json";
+
+/**
+ * The data for the website.
+ * Use loadData to populate.
+ */
 let data = {};
 
+/**
+ * Elements from the HTML.
+ */
 const elements = {
     selectDataset : d3.select("#selDataset"),
     divSampleMetadata : d3.select("#sample-metadata"),
@@ -9,10 +20,14 @@ const elements = {
     divBubble : d3.select("#bubble"),
 }
 
-function optionChanged(name)
+/**
+ * Onchange handler for selDataset.
+ * @param {string} id - The test subject ID selected.
+ */
+function optionChanged(id)
 {
-    const samples = data.samples.find(sample => sample.id === name);
-    const metadata = data.metadata.find(subdata => `${subdata.id}` === name);
+    const samples = data.samples.find(sample => sample.id === id);
+    const metadata = data.metadata.find(subdata => `${subdata.id}` === id);
 
     populateSampleMetadata(metadata);
     populateBar(samples);
@@ -20,26 +35,33 @@ function optionChanged(name)
     populateBubble(samples);
 }
 
+/**
+ * Loads a JSON file for use in the code.
+ * @param {string} jsonFile - The filepath for the JSON file.
+ */
 async function loadData(jsonFile)
 {
     const dataset = await d3.json(jsonFile);
     return dataset;
 }
 
-function getTopTenSamples(samples)
-{
-
-}
-
-function populateSelectDataset(names)
+/**
+ * Adds options to the selDataset selection element.
+ * @param {string[]} options - The options to add.
+ */
+function populateSelectDataset(options)
 {
     const selectDataset = elements.selectDataset;
-    names.forEach(name => {
-        const option = selectDataset.append("option");
-        option.text(name);
+    options.forEach(option => {
+        const optionElement = selectDataset.append("option");
+        optionElement.text(option);
     });
 }
 
+/**
+ * Creates a table within the sample-metadata div.
+ * @param {any[]} metadata - The metadata to populate.
+ */
 function populateSampleMetadata(metadata)
 {
     const divSampleMetadata = elements.divSampleMetadata;
@@ -65,9 +87,36 @@ function populateSampleMetadata(metadata)
     });
 }
 
+/**
+ * Creates a top bar chart for the samples.
+ * @param {any[]} samples - The sample data to show.
+ */
 function populateBar(samples)
 {
+    const topIDs = samples.otu_ids.slice(0, 10).reverse();
+    const topLabels = samples.otu_labels.slice(0, 10).reverse();
+    const topValues = samples.sample_values.slice(0, 10).reverse();
 
+    const otuTrace = 
+    {
+        name : "Top OTUs",
+        type : "bar",
+        orientation : "h",
+        y: topIDs.map(id => `[${id}]`),
+        x: topValues,
+        text: topLabels
+    }
+
+    const traces = [otuTrace];
+
+    const layout = 
+    {
+        title : `Top OTUs for Test Subject ${samples.id}`,
+        yaxis : { title : "OTU ID"},
+        xaxis : { title : "Sample Values"}
+    }
+
+    Plotly.newPlot(elements.divBar.node(), traces, layout);
 }
 
 function populateGauge(samples)
@@ -83,7 +132,6 @@ function populateBubble(samples)
 async function init()
 {
     data = await loadData(samplesJsonFile);
-    console.log(data);
     populateSelectDataset(data.names);
     optionChanged(data.names[0]);
 }
